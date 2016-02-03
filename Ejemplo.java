b588e0b48cf38e912e815dc9dafea8c72de0f9ee
Jose Diego <DIEGOmai1995@hotmail.com>
2016-02-02 22:55:13 -0600

    Version con panel en el centro

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,14 +1,25 @@
+//Bibliotecas.
 import javax.swing.*;
 import java.awt.*;
-
+//Clase Ejemplo.
 public class Ejemplo extends JFrame{
+protected JPanel p;
 	public Ejemplo(){
+		p = new JPanel();
 		setLayout(new BorderLayout());
 		add("North", new JButton("Norte."));
  		add("South", new JButton("Sur."));
  		add("East", new JButton("Este."));
  		add("West", new JButton("Oeste."));
-		add("Center", new JButton("Centro."));
+		p.setLayout(new BorderLayout());
+		
+		p.add("Center",new JButton("Boton."));
+                p.add("South", new JLabel("Sur.",JLabel.CENTER));
+                p.add("East", new JLabel("Este."));
+                p.add("West", new JLabel("Oeste."));
+		p.add("North", new JLabel("Norte.",JLabel.CENTER));
+
+		add("Center", p);
  		setVisible(true);
 		setBounds(0,0,500,500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
